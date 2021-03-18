ef426d2353a9d54804a3cdd33ee8176032b0b2f9
msh01 <yingwaizhiying@gmail.com>
2021-03-18 17:47:28 +0800

    feat(代码生成): 临时提交

--- yoma-code-gen/src/main/java/com/github/yoma/codegen/utils/GenUtil.java
+++ yoma-code-gen/src/main/java/com/github/yoma/codegen/utils/GenUtil.java
@@ -49,6 +49,7 @@ public class GenUtil {
     public static final String EXTRA = "auto_increment";
 
     public static final String generateTargetDir = "D:\\yoma-generate";
+    public static final Boolean camel_case_enable = false;
 
     /**
      * 获取后端代码模板名称
@@ -130,7 +131,7 @@ public class GenUtil {
         List<String> templates = getAdminTemplateNames();
         for (String templateName : templates) {
             Template template = engine.getTemplate("generator/admin/" + templateName + ".ftl");
-            String filePath = getAdminFilePath( templateName, genConfig, genMap.get("className").toString(),
+            String filePath = getAdminFilePath(templateName, genConfig, genMap.get("className").toString(),
                     rootPath);
             assert filePath != null;
             File file = new File(filePath);
@@ -271,7 +272,12 @@ public class GenUtil {
             // 主键类型
             String colType = ColUtil.cloToJava(column.getColumnType());
             // 小写开头的字段名
-            String changeColumnName = StringUtils.toCamelCase(column.getColumnName());
+            String changeColumnName = null;
+            changeColumnName = StringUtils.toCamelCase(column.getColumnName());
+            // if (camel_case_enable) {
+            // } else {
+            //     changeColumnName = column.getColumnName();
+            // }
             // 大写开头的字段名
             String capitalColumnName = StringUtils.toCapitalizeCamelCase(column.getColumnName());
             if (PK.equals(column.getKeyType())) {
